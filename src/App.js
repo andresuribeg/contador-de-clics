c121a7359609fededb324af2d2a8eb0686f3d9b5
Andres Uribe G <145047387+andresuribeg@users.noreply.github.com>
2023-12-26 16:05:29 -0500

    Crear el botón  03:05:07 (#1)
    Co-authored-by: Andres Uribe <neluri@comfenalcoquindio.co>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import freeCodeCampLogo from "./img/freecodecamp-logo.png";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freeCodeCampLogo}
+          alt="Logo de freecode camp"
+        />
+      </div>
+      <div className="contenedor-principal">
+        
+      </div>
     </div>
   );
 }
